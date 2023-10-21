7e96f2183cafe0bc5988f91ffd6730c32a2c16c3
sharifulislamshihan <sharifulislamshihan@gmail.com>
2023-10-22 01:36:14 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import {
+    initializeApp
+} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDZHhsAlUxpdpZ8M7-f91-1I4hIuBOYI0I",
+    authDomain: "wedding-event-management-4eea7.firebaseapp.com",
+    projectId: "wedding-event-management-4eea7",
+    storageBucket: "wedding-event-management-4eea7.appspot.com",
+    messagingSenderId: "842078475728",
+    appId: "1:842078475728:web:076817dcc2719ec9a795d5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
