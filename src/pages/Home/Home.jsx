aa52582b72947135f1b791ba8ebdf8641193d613
sharifulislamshihan <sharifulislamshihan@gmail.com>
2023-10-23 04:08:45 +0600

    footer done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,28 @@
-import { NavLink, Navigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
 import Carousel from "./Carousel";
 
+// Import AOS
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
+
 
 const Home = () => {
+
+    useEffect(() => {
+        AOS.init();
+    }, [])
+
     return (
         <div>
             <Navbar></Navbar>
             <Carousel></Carousel>
             {/* View Services */}
             <section>
-                <div className="grid grid-cols-2 gap-16 my-28 ">
-                    <div className="mt-12">
+                <div className="grid grid-rows-2 md:grid-cols-2 gap-16 my-28 ">
+                    <div data-aos="fade-right" data-aos-duration="1200" className="mt-12 ml-3 mr-3">
                         <h3 className="text-5xl font-heading font-bold">THE EXPERIENCE</h3>
                         <h4 className="text-3xl font-heading font-semibold my-4">ALL INCLUSIVE WEDDING PACKAGES</h4>
                         <p className="text-xl font-paragraph font-semibold">The Farmhouse provides an intimate setting for your wedding ceremony and reception, all in one convenient location.</p>
@@ -24,7 +34,7 @@ const Home = () => {
                         </div>
 
                     </div>
-                    <div>
+                    <div data-aos="fade-left" data-aos-duration="1200">
                         <img className="" src="/src/assets/image/Service/service heading/service-heading.jpg" alt="" />
                     </div>
                 </div>
--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -3,7 +3,23 @@
 const Footer = () => {
     return (
         <div>
-            <h3 className="text-2xl">This is footer</h3>
+            <footer className="footer p-10 text-black  text-neutral-content">
+                <aside>
+                    <img className=" w-2/4 " src="/src/assets/image/Logo/logo.jpg" alt="" />
+                    <p className="text-3xl font-paragraph">Wedding Planner.<br />Providing reliable tech since 1992</p>
+                </aside>
+                <nav className=" mt-0 md:mt-36 ">
+                    <header className="footer-title">Social</header>
+                    <div className="grid grid-flow-col gap-4">
+                        <a href="facebook.com"><img className="w-10" src="/src/assets/image/Logo/facebook.png" alt="" /></a>
+                        <a href="instagram.com"><img className="w-10" src="/src/assets/image/Logo/instagram.png" alt="" /></a>
+                        <a href="youtube.com"><img className="w-12" src="/src/assets/image/Logo/youtube.png" alt="" /></a>
+                    </div>
+                </nav>
+            </footer>
+            
+                <p className="text-2xl font-heading font-bold text-center mb-5">Copyright © 2023 - All right reserved by <span className="font-paragraph">Wedding Planner</span></p>
+            
         </div>
     );
 };
