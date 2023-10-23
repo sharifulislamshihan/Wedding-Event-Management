8d47d295dbb8389f0e49d354c416122a72239b80
sharifulislamshihan <sharifulislamshihan@gmail.com>
2023-10-23 12:02:02 +0600

    venue section in home page added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,25 +20,45 @@ const Home = () => {
             <Navbar></Navbar>
             <Carousel></Carousel>
             {/* View Services */}
-            <section>
-                <div className="grid grid-rows-2 md:grid-cols-2 gap-16 my-28 ">
-                    <div data-aos="fade-right" data-aos-duration="1200" className="mt-12 ml-3 mr-3">
-                        <h3 className="text-5xl font-heading font-bold">THE EXPERIENCE</h3>
-                        <h4 className="text-3xl font-heading font-semibold my-4">ALL INCLUSIVE WEDDING PACKAGES</h4>
-                        <p className="text-xl font-paragraph font-semibold">The Farmhouse provides an intimate setting for your wedding ceremony and reception, all in one convenient location.</p>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 mt-28 mx-14 ">
+                <div data-aos="fade-right" data-aos-duration="1200" className="mt-6 ml-3 mr-3">
+                    <h3 className="text-5xl font-heading font-bold">THE EXPERIENCE</h3>
+                    <h4 className="text-3xl font-heading font-semibold my-4">ALL INCLUSIVE WEDDING PACKAGES</h4>
+                    <p className="text-xl font-paragraph font-semibold">The Farmhouse provides an intimate setting for your wedding ceremony and reception, all in one convenient location.</p>
 
-                        <p className="text-xl font-paragraph font-semibold">The all inclusive wedding package creates a stress free experience for your perfect wedding day. The packages include a premium open bar, gourmet food prepared onsite, linens & napkins in your choice of color, candle centerpieces, wedding cake, hotel accommodations, wedding coordinator & more</p>
+                    <p className="text-xl font-paragraph font-semibold mt-3">The all inclusive wedding package creates a stress free experience for your perfect wedding day. The packages include a premium open bar, gourmet food prepared onsite, linens & napkins in your choice of color, candle centerpieces, wedding cake, hotel accommodations, wedding coordinator & more</p>
 
-                        <div className="my-5">
-                            <NavLink to='/services'><button className="font-heading border border-double border-[#ae9a64] text-[#ae9a64] text-2xl font-semibold px-7 py-3 rounded-lg hover:bg-[#ae9a64] hover:text-black">VIEW OUR SERVICES</button></NavLink>
-                        </div>
-
+                    <div className="my-5">
+                        <NavLink to='/services'><button className="font-heading border border-double border-[#ae9a64] text-[#ae9a64] text-2xl font-semibold px-7 py-3 rounded-lg hover:bg-[#ae9a64] hover:text-black">VIEW OUR SERVICES</button></NavLink>
                     </div>
-                    <div data-aos="fade-left" data-aos-duration="1200">
-                        <img className="" src="/src/assets/image/Service/service heading/service-heading.jpg" alt="" />
-                    </div>
+
                 </div>
-            </section>
+                <div data-aos="fade-left" data-aos-duration="1200">
+                    <img className="" src="/src/assets/image/Service/service heading/service-heading.jpg" alt="" />
+                </div>
+            </div>
+
+
+            {/* view about */}
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-16 my-28 mx-14">
+                <div data-aos="fade-up-right" data-aos-duration="1200">
+                    <img className="" src="/src/assets/image/venue.jpg" alt="" />
+                </div>
+                <div data-aos="fade-up-left" data-aos-duration="1200" className=" ml-3 mr-3 text-end">
+                    <h3 className="text-5xl font-heading font-bold">THE VENUE</h3>
+                    <h4 className="text-3xl font-heading font-semibold my-4">BEAUTIFULLY RESTORED FARMHOUSE & ESTATE</h4>
+                    <p className="text-xl font-paragraph font-semibold">The Farmhouse offers intimate indoor spaces for your event including the bright and open main terrace hall, indoor lounge and bar, dressing suites and more. Outside, a large wrap around  deck and patio overlook the gazebo bar & gardens.</p>
+
+                    <p className="text-xl font-paragraph font-semibold mt-3">The historical estate boasts a picturesque outdoor ceremony site underneath romantic willow trees & four acres of manicured
+                        grounds and gardens.</p>
+
+                    <div className="my-5">
+                        <NavLink to='/about'><button className="font-heading border border-double border-[#ae9a64] text-[#ae9a64] text-2xl font-semibold px-7 py-3 rounded-lg hover:bg-[#ae9a64] hover:text-black">ABOUT</button></NavLink>
+                    </div>
+
+                </div>
+            </div>
             <Footer></Footer>
         </div>
     );
